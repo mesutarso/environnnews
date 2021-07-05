754988b832bbb0c7c443ab62ecb22b4f23b33eaf
Eminence <mulemanowa@gmail.com>
2021-07-05 22:49:01 +0200

    feature : prettier configuration file add

--- test.format.tsx
+++ test.format.tsx
@@ -1,5 +1,5 @@
-const array = ["bonjour", "au revoir", 22, 3232.3, 32];
-const direBonjour = (element) => element;
+var array = ["bonjour", "au revoir", 22, 3232.3, 32];
+var direBonjour = (element) => element;
 for (let i = 0; i < array.length; i += 1) {
   const element = array[i];
   console.log(direBonjour(element));
