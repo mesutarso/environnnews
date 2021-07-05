e5749f19394de60928ed1a092b8766584ee0d894
Eminence <mulemanowa@gmail.com>
2021-07-05 23:03:56 +0200

    feature : vscode configuration file add

--- test.format.tsx
+++ test.format.tsx
@@ -1,6 +1,7 @@
-var array = ['bonjour', 'au revoir', 22, 3232.3, 32]
-var direBonjour = (element) => element
+const array = ['bonjour', 'au revoir', 22, 3232.3, 32]
+const direBonjour = (element) => element
 for (let i = 0; i < array.length; i += 1) {
 	const element = array[i]
 	console.log(direBonjour(element))
 }
+export{}
