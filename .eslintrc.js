a726a50f0e66b8caf959aeef7953d0ea5fb6ccf6
Eminence <mulemanowa@gmail.com>
2021-07-06 05:00:38 +0200

    feature : pre-commit configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb','prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'airbnb',
+		'prettier',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: false,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'new-cap': 'off',
+		'lines-around-comment': 'off',
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+	},
 };
--- test.format.tsx
+++ test.format.tsx
@@ -1,7 +1,7 @@
-const array = ['bonjour', 'au revoir', 22, 3232.3, 32]
-const direBonjour = (element) => element
+const array = ['bonjour', 'au revoir', 22, 3232.3, 32];
+const direBonjour = (element) => element;
 for (let i = 0; i < array.length; i += 1) {
-	const element = array[i]
-	console.log(direBonjour(element))
+	const element = array[i];
+	console.log(direBonjour(element));
 }
-export{}
+export {};
