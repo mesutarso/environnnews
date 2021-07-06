459ea30b7cee18f5b35de039d1a40ca5398458da
Eminence <mulemanowa@gmail.com>
2021-07-06 05:24:17 +0200

    feature : config continious integrtation added

--- test.format.tsx
+++
@@ -1,7 +0,0 @@
-const array = ['bonjour', 'au revoir', 22, 3232.3, 32];
-const direBonjour = (element) => element;
-for (let i = 0; i < array.length; i += 1) {
-	const element = array[i];
-	console.log(direBonjour(element));
-}
-export {};
