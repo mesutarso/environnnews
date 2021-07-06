dfcc12f833be7625c5d57f1fffdb712c8e6687b5
root <root@localhost.localdomain>
2021-07-06 15:25:25 +0100

    feat: add footer component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
 	return (
 		<div>
 			<Header />
 			{children}
+			<Footer />
 		</div>
 	);
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
