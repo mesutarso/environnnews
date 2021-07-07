a4fc923af03e107be1bfc39fc5e0166e57805a8e
root <root@localhost.localdomain>
2021-07-07 12:01:15 +0100

    feature : add category section

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -3,14 +3,14 @@ import { IArticles } from '../pages';
 
 import articleStyles from '../styles/Article.module.css';
 
-export const ArticleCard = ({ article }) => {
+export const ArticleCard = ({ article, size, imageHeight, imageWidth }) => {
 	return (
-		<div className='col-md-4 col-12'>
+		<div className={`col-md-${size} col-12 ${articleStyles.card}`}>
 			<Image
 				src={article.image}
 				alt={article.description}
-				width={400}
-				height={300}
+				width={imageWidth}
+				height={imageHeight}
 			/>
 			<h5 className={`text-success ${articleStyles.title}`}>
 				{article.category}
@@ -26,7 +26,13 @@ const Articles: React.FC<IArticles> = ({ articles }) => {
 	return (
 		<div className='row d-flex justify-content-center'>
 			{articles.map((article) => (
-				<ArticleCard key={article.id} article={article} />
+				<ArticleCard
+					key={article.id}
+					article={article}
+					size={6}
+					imageHeight={250}
+					imageWidth={380}
+				/>
 			))}
 		</div>
 	);
