75d2747ae009affd9d971faeb20d60404062b5f9
Eminence <mulemanowa@gmail.com>
2021-07-09 21:43:39 +0200

    feat : Banner  test added

--- __tests__/Partner.test.js
+++ __tests__/Partner.test.js
@@ -1,19 +1,12 @@
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent, cleanup } from '../test-utils';
-import PartnerSection from '../components/sect';
-import { title } from 'process';
+import PartnerSection from '../components/Partner';
 
 let getByTestId;
-const partner = [
-	{
-		id: 1,
-		image: '/assets/afd1.png',
-		alt: 'logo de partner afd 1',
-	},
-];
+
 beforeEach(() => {
-	const component = render(<PartnerSection partner={parter} />);
+	const component = render(<PartnerSection />);
 	getByTestId = component.getByTestId;
 });
 
@@ -26,14 +19,5 @@ describe('Partner test', () => {
 		const titleSection = getByTestId('section-partner-title');
 		expect(titleSection.className).not.toBeNull();
 		expect(titleSection.textContent).toBe('ILS NOUS FONT CONFIANCE');
-		expect(titleSection.className).not.toBeNull();
-	});
-	it('should show partner image', () => {
-		const imagePartner = getByTestId('image-partner');
-		expect(imagePartner.className).not.toBeNull();
-		expect(imagePartner.src).not.toBeNull();
-		expect(imagePartner.alt).not.toBeNull();
-		expect(imagePartner.src).toContain('/assets/afd1.png');
-		expect(imagePartner.alt).toContain('logo de partner afd 1');
 	});
 });
