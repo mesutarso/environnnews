558a3c9df7328d9175768e73719bd71ff4c167eb
Eminence <mulemanowa@gmail.com>
2021-07-09 14:25:36 +0200

    feat : the display of the logo in the footer tested

--- __tests__/Footer.test.js
+++ __tests__/Footer.test.js
@@ -1,19 +1,30 @@
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
-import { render, screen } from '../test-utils';
+import { render, screen, fireEvent, cleanup } from '../test-utils';
 import Footer from '../components/Footer';
 
+let getByTestId;
+beforeEach(() => {
+	const component = render(<Footer />);
+	getByTestId = component.getByTestId;
+});
+
+afterEach(() => {
+	cleanup();
+});
 describe('Footer', () => {
-	test('should render the heading', () => {
+	it('should render the heading', () => {
 		render(<Footer />);
 		const titles = screen.getAllByRole('heading', { level: 6 });
 		for (const title of titles) {
 			expect(title).toBeInTheDocument();
 		}
 	});
-	// test('should render the link', () => {
-	// 	render(<Footer />);
-	// 	const links = screen.getAllByText(/\//i);
-	// 	expect(links).toContain();
-	// });
+
+	it('should show a logo', () => {
+		const logoEl = getByTestId('logo-environews');
+		expect(logoEl.className).not.toBeNull();
+		expect(logoEl.src).toContain('/assets/environews_logo.png');
+		expect(logoEl.alt).toContain('logo d\'environ news');
+	});
 });
