26efb356e44f70805ff6c72305f2d874fa07e83b
Béni MAMPUNINA <contactmampbeni@gmail.com>
2021-07-09 18:33:37 +0100

    fix: fix linking

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,21 @@ import Header from '../components/Header';
 import PartnerSection from '../components/Partner';
 import Footer from './Footer';
 
-export default function Layout({ children }) {
+export default function Layout({ children, id }) {
 	return (
 		<div>
-			<Header />
+			<Header ip={id} />
 			{children}
 			<PartnerSection />
 			<Footer />
 		</div>
 	);
 }
+
+export const getStaticProps = () => {
+	return {
+		props: {
+			id: 1,
+		},
+	};
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, id }) {
 	return (
-		<Layout>
+		<Layout id={id}>
 			<Component {...pageProps} />
 		</Layout>
 	);
 }
 
 export default MyApp;
+
+export const getStaticProps = () => {
+	return {
+		props: {
+			id: 150,
+		},
+	};
+};
